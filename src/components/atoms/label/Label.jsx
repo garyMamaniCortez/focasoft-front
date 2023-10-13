1a472675760a8fb93cbaebc2f5221760fb628b1b
luis <rosalescaero@gmail.com>
2023-10-13 07:50:52 -0400

    styles: se modificaron los estilos del comopente Label

--- src/components/atoms/label/Label.jsx
+++ src/components/atoms/label/Label.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './Label.css'
 
 const Label = ({LabelType, children}) => {
     return (
-      <label  className={`${LabelType == "FormLabel" ? "FormLabel" : ""}
-      ${LabelType == "FormTitle" ? "FormTitle" : ""}
-      ${LabelType == "ListBody" ? "ListBody" : ""}
+      <label  className={`${LabelType === "FormLabel" ? "FormLabel" : ""}
+      ${LabelType === "FormTitle" ? "FormTitle" : ""}
+      ${LabelType ==="ListBody" ? "ListBody" : ""}
       `}>
         {children}
       </label>
