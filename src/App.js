a78cd0cf9ab16ca9876a636ad2c02fc64d5d4ca7
luis <rosalescaero@gmail.com>
2023-10-13 14:57:44 -0400

    feat: se implementado la seccion para crear evento

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import CreateEventForm from "./components/molecules/createEvent/CreateEventForm";
-
-const campos = [
-  { etiqueta: 'TItulo del evento', LabelType: 'FormLabel', InputType: 'text', Select: false },
-  { etiqueta: 'Fecha del evento', LabelType: 'FormLabel', InputType: 'date', Select: false  },
-  { etiqueta: 'Tipo del evento', LabelType: 'FormLabel', InputType: 'text', Select: true  },
-  { etiqueta: 'Descripcion', LabelType: 'FormLabel', InputType: 'description', Select: false  },
-  { etiqueta: 'Afiche del evento', LabelType: 'FormLabel', InputType: 'text', Select: false  },
-  { etiqueta: 'Requsitos', LabelType: 'FormLabel', InputType: 'text', Select: false  },
-  { etiqueta: 'Premios', LabelType: 'FormLabel', InputType: 'text'  },
-  { etiqueta: 'Patrocinadores', LabelType: 'FormLabel', InputType: 'text', Select: false  },
-  { etiqueta: 'Contactos', LabelType: 'FormLabel', InputType: 'text', Select: 'false'},
-];
+import CreateEventSection from "./components/organisms/createEventSection/CreateEventSection";
 
 function App() {
 
   return (
     <div>
-      <CreateEventForm campos= {campos}/>
+      <CreateEventSection/>
     </div>
   );
 }
