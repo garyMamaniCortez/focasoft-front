913d31ddc4e1b8e4dcb798ef068d677c56c0c671
4drianC4 <camararequeadrianrafael@gmail.com>
2023-10-13 02:12:00 -0400

    feat: Implementar el componente Boton y aplicar funciones condicionales de estilo
    En este commit:
    - Se implementó el componente Boton, agregando el archivo correspondiente y las funcionalidades necesarias.
    - Se aplicaron condiciones de estilo dinámicas en la función Boton para adaptar su apariencia según ciertas condiciones, mejorando la flexibilidad del componente.
    
    Detalles adicionales:
    - Se agregó el archivo 'Boton.js' que contiene la implementación del componente Boton.
    - Se utilizaron condicionales de estilo para manejar cambios dinámicos en el color,

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Label from "./components/atoms/label/Label";
 import "./components/atoms/label/Label.css"
-
+import Boton from "./components/atoms/boton/Boton.jsx"
 function App() {
   return (
     <div>
@@ -8,6 +8,7 @@ function App() {
       <br/><Label LabelType="ListBody" >
         Gato
       </Label><br/>
+      <Boton text='Holi' buttonType='crudButton'/>
     </div>
   );
 }
