a2e4e90f490e83abd732dea943fab0544a624cc3
N-th7 <64654131+N-th7@users.noreply.github.com>
2023-10-13 20:20:00 -0400

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import Label from "./components/atoms/label/Label";
-import "./components/atoms/label/Label.css"
-import Boton from "./components/atoms/boton/Boton.jsx"
+
 function App() {
   return (
     <div>
-      Esta es una etiqueta
-      <br/><Label LabelType="ListBody" >
-        Gato
-      </Label><br/>
-      <Boton text='Holi' buttonType='crudButton'/>
+      App      
     </div>
   );
 }
