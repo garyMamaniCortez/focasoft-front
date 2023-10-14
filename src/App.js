2bb4f961b8f02b419a7733e59a5e918724275994
luis <rosalescaero@gmail.com>
2023-10-14 11:36:35 -0400

    style:se arreglo la alineacion de los inputs en el form para crear evento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import CreateEvent from "./components/templates/createEvent/CreateEvent"
 import VistaEventos from "./components/templates/VistaEventos/VistaEventos";
 import Navbar from "./components/organisms/navbar/Navbar"
+import CreateRegister from "./components/templates/createRegister/CreateRegister"
+
 import "w3-css"
 function App() {
 
@@ -8,6 +10,7 @@ function App() {
     <div className="w3-row">
         <Navbar/>
       <div className="l10 w3-col w3-right">
+        <CreateEvent></CreateEvent>
       </div>
     </div>
   );
