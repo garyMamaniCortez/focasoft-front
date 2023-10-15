4dad2af7ed81b3dc8eb2fd8c6bbf66538edb4f95
luis <rosalescaero@gmail.com>
2023-10-15 13:57:02 -0400

    feat: EL formulario para vrear evento regresa los valores de los campos en un arreglo

--- src/components/organisms/createEventSection/CreateEventSection.jsx
+++ src/components/organisms/createEventSection/CreateEventSection.jsx
@@ -1,22 +1,53 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import Formulario from "../../molecules/formulario/Formulario.jsx";
 import Boton from "../../atoms/boton/Boton.jsx"
 import Background from "../../atoms/background/Background.jsx"; 
-import { Link } from "react-router-dom";
 
-const CreateEventSection = ({CamposDeEntrada}) => {
+const CreateEventSection = () => {
+
+  const CamposDeEntrada = [
+    { Etiqueta: 'TItulo del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'TituloDelEvento'},
+    { Etiqueta: 'Fecha del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'date', Identificador:'FechaDelEvento'},
+    { Etiqueta: 'Tipo del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'select', Identificador:'TipoDelEvento'},
+    { Etiqueta: 'Descripcion', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'description', Identificador:'Descripcion'},
+    { Etiqueta: 'Afiche del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'file', Identificador:'AficheDelEvento'},
+    { Etiqueta: 'Requsitos', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'Requsitos'},
+    { Etiqueta: 'Premios', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'Premios'},
+    { Etiqueta: 'Patrocinadores', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'Patrocinadores'},
+    { Etiqueta: 'Contactos', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'Contactos'},
+  ];
+
+  const [formData, setFormData] = useState({TituloDelEvento: "",FechaDelEvento: "", TipoDelEvento: ""
+  , Descripcion: "",AficheDelEvento: "",Requsitos: "",Premios: "", Patrocinadores:"", Contactos:""});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Titulo Del Evento: ${formData.TituloDelEvento} 
+    \nDescripcion: ${formData.TituloDelEvento}
+    \nTipo del evento: ${formData.TipoDelEvento}  `
+    );
+};
 
   return (
       <Background>
-      <Formulario CamposDeEntrada= {CamposDeEntrada}/>
-        <div className="w3-row w3-center">
-          <div className="createEventButton w3-col l6">
-          <Link to="/CrearFormulario"><Boton text="Formulario de registro" buttonType="formButton"></Boton></Link>
+        <form onSubmit={handleSubmit}>
+        <Formulario CamposDeEntrada= {CamposDeEntrada} handleChange = {handleChange}/>
+          <div className="w3-row w3-center">
+            <div className="createEventButton w3-col l6">
+            <Link to="/CrearFormulario"><Boton text="Formulario de registro" buttonType="formButton"></Boton></Link>
+            </div>
+            <div className="w3-col l6">
+            <button type="submit">Probar</button>
+            </div>
           </div>
-          <div className="w3-col l6">
-          <Boton text="Crear evento" buttonType="crudButton"></Boton>
-          </div>
-        </div>
-
+          </form>
       </Background>
   );
 }
--- src/components/templates/createEvent/CreateEvent.jsx
+++ src/components/templates/createEvent/CreateEvent.jsx
@@ -1,24 +1,12 @@
 import CreateEventSection from "../../organisms/createEventSection/CreateEventSection";
 import './CreateEvent.css'
 const CreateEvent = () => {
-
-    const CamposDeEntrada = [
-        { Etiqueta: 'TItulo del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'1'},
-        { Etiqueta: 'Fecha del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'date', Identificador:'2'},
-        { Etiqueta: 'Tipo del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'select', Identificador:'3'},
-        { Etiqueta: 'Descripcion', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'description', Identificador:'4'},
-        { Etiqueta: 'Afiche del evento', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'file', Identificador:'5'},
-        { Etiqueta: 'Requsitos', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'6'},
-        { Etiqueta: 'Premios', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'7'},
-        { Etiqueta: 'Patrocinadores', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'8'},
-        { Etiqueta: 'Contactos', TipoDeEtiqueta: 'FormLabel', TipoDeEntrada: 'text', Identificador:'9'},
-      ];
       
     return (
       <div className="CreateEventContent vistaContent w3-right">
         <h1 className="H1CreateEvent">Crear Evento</h1>
         <div className="CreateEventSection">
-          <CreateEventSection CamposDeEntrada={CamposDeEntrada}/>
+          <CreateEventSection/>
         </div>
       </div>
     );
