f74b444e8904c918391de5d98bbdcf23db8f8aa4
N-th7 <64654131+N-th7@users.noreply.github.com>
2023-10-15 12:32:38 -0400

    feat: integracion vista eventos con base de datos

--- src/components/organisms/CardSection/CardSection.jsx
+++ src/components/organisms/CardSection/CardSection.jsx
@@ -1,31 +1,49 @@
 import Card from "../../molecules/card/Card";
 import 'w3-css/w3.css';
 import "./cardSection.css"
+import axios from "axios"
+
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { async } from "q";
+import { useState } from "react";
+
+
+const endpoint = 'http:///localhost:8000/api'
+
 function CardSection(){
+    const [eventos, setEventos] = useState( [] );
+    useEffect(()=>{
+        getAllEvents()
+    },[])
+
+    const getAllEvents = async () => {
+        const response = await axios.get("http://localhost:8000/api/eventos")
+        setEventos(response.data)
+        console.log(response.data)
+    }
+
     return(
         <div className="cardSectionContent">
             <h2 className="cardSTi">Eventos</h2>
             <div className="w3-row cards">
-                <div className="w3-col l6 m6 card">
-                    <Card 
-                        category="Competencia" 
-                        title="Competencia de clasificación UMSS" 
-                        date="05/10/2023"
-                        description="Lorem ipsum dolor sit amet consectetur. Adipiscing diam odio odio feugiat tincidunt elementum tellus. Eget facilisis purus consequat convallis nec. Ut euismod curabitur amet elementum tellus etiam lorem. Orci pharetra non suspendisse integer diam aliquet mi ac faucibus."
-                        src="https://www.umss.edu.bo/wp-content/uploads/2023/03/ICPC-Bolivia-Regionals-2022-afiche.png">
+                {
+                eventos.map( ( evento ) => (
 
-                    </Card>
-                </div>
-                <div className="w3-col l6 m6 card">
+                    <div className="w3-col l6 m6 card" key={evento.id}>
                     <Card 
-                        category="Competencia" 
-                        title="Competencia de clasificación UMSS" 
-                        date="05/10/2023"
-                        description="Lorem ipsum dolor sit amet consectetur. Adipiscing diam odio odio feugiat tincidunt elementum tellus. Eget facilisis purus consequat convallis nec. Ut euismod curabitur amet elementum tellus etiam lorem. Orci pharetra non suspendisse integer diam aliquet mi ac faucibus."
+                        category={evento.tipo}
+                        title={evento.titulo} 
+                        date={evento.fecha_ini}
+                        description={evento.descripcion}
                         src="https://www.umss.edu.bo/wp-content/uploads/2023/03/ICPC-Bolivia-Regionals-2022-afiche.png">
-
+    
                     </Card>
-                </div>
+                    </div>
+
+                ))
+                }
+                
             </div>
 
         </div>
