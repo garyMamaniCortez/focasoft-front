19fb14b0a3c5ae73111a034df69a4f9322092bbe
luis <rosalescaero@gmail.com>
2023-11-13 20:11:59 -0400

    Formulario dinamico: se implemento un formulario de registro para las competencias

--- src/components/atoms/textInput/TextInput.jsx
+++ src/components/atoms/textInput/TextInput.jsx
@@ -10,79 +10,84 @@ const TextInput = ({
 }) => {
   return (
     <>
-      <input
-        id={TipoDeEntrada !== "select" ? Identificador : ""}
-        name={TipoDeEntrada !== "select" ? Identificador : ""}
-        className={`${
-          TipoDeEntrada === "text"
-            ? "EntradaDeTexto"
-            : TipoDeEntrada === "date"
-            ? "EntradaDeTexto"
-            : TipoDeEntrada === "description"
-            ? "EntradaDeTexto"
-            : TipoDeEntrada === "file"
-            ? "EntradaDeTexto"
-            : TipoDeEntrada === "select"
-            ? "EntradaDeTexto"
-            : TipoDeEntrada === "email"
-            ? "EntradaDeTexto"
-            : TipoDeEntrada === "password"
-            ? "Contrasena"
-            : TipoDeEntrada === "User"
-            ? "User"
-            : ""
-        }
-        `}
-        type={`${
-          TipoDeEntrada === "text"
-            ? "text"
-            : TipoDeEntrada === "description"
-            ? "text"
-            : TipoDeEntrada === "date"
-            ? "date"
-            : TipoDeEntrada === "file"
-            ? "file"
-            : TipoDeEntrada === "email"
-            ? "email"
-            : TipoDeEntrada === "password"
-            ? "password"
-            : TipoDeEntrada === "User"
-            ? "text"
-            : ""
-        }`}
-
-        placeholder={`${TipoDeEntrada === "password" ? "contraseña" : 
-                        TipoDeEntrada === "User" ? "usuario" : ""}`}
-
-        accept={`${TipoDeEntrada === "file" ? ".jpeg" : ""}`}
-        onChange={ManejarCambio}
-        disabled={
-          Desactivado ? true : TipoDeEntrada === "select" ? true : false
-        }
-        hidden={TipoDeEntrada === "select" ? true : false}
-        value={
-          TipoDeEntrada === "file"
-            ? ""
-            : TipoDeEntrada === "Select"
-            ? ""
-            : Valor 
-        }
-      />
-      <select
-        id={TipoDeEntrada === "select" ? Identificador : ""}
-        name={TipoDeEntrada === "select" ? Identificador : ""}
-        className="EntradaDeTexto"
-        disabled={
-          Desactivado ? true : TipoDeEntrada !== "select" ? true : false
-        }
-        onChange={ManejarCambio}
-        hidden={TipoDeEntrada !== "select" ? true : false}
-        value={TipoDeEntrada !== "select" ? "" : Valor}
-      >
-        {OpcionesDelDesplegable.map((item) => (
-          <option value={item.Valor}>{item.Etiqueta}</option>
-        ))}
-      </select>
+      {TipoDeEntrada === "text" ? (
+        <input
+          id={Identificador}
+          name={Identificador}
+          className={`${
+            TipoDeEntrada === "text"
+              ? "EntradaDeTexto"
+              : TipoDeEntrada === "date"
+              ? "EntradaDeTexto"
+              : TipoDeEntrada === "description"
+              ? "EntradaDeTexto"
+              : TipoDeEntrada === "file"
+              ? "EntradaDeTexto"
+              : TipoDeEntrada === "select"
+              ? "EntradaDeTexto"
+              : TipoDeEntrada === "email"
+              ? "EntradaDeTexto"
+              : TipoDeEntrada === "password"
+              ? "Contrasena"
+              : TipoDeEntrada === "User"
+              ? "User"
+              : ""
+          }
+    `}
+          type={`${
+            TipoDeEntrada === "text"
+              ? "text"
+              : TipoDeEntrada === "description"
+              ? "text"
+              : TipoDeEntrada === "date"
+              ? "date"
+              : TipoDeEntrada === "file"
+              ? "file"
+              : TipoDeEntrada === "email"
+              ? "email"
+              : TipoDeEntrada === "password"
+              ? "password"
+              : TipoDeEntrada === "User"
+              ? "text"
+              : ""
+          }`}
+          placeholder={`${
+            TipoDeEntrada === "password"
+              ? "contraseña"
+              : TipoDeEntrada === "User"
+              ? "usuario"
+              : ""
+          }`}
+          accept={`${TipoDeEntrada === "file" ? ".jpeg" : ""}`}
+          onChange={ManejarCambio}
+          disabled={
+            Desactivado ? true : TipoDeEntrada === "select" ? true : false
+          }
+          value={
+            TipoDeEntrada === "file"
+              ? ""
+              : TipoDeEntrada === "Select"
+              ? ""
+              : Valor
+          }
+        />
+      ) : (
+        <select
+          id={TipoDeEntrada === "select" ? Identificador : ""}
+          name={TipoDeEntrada === "select" ? Identificador : ""}
+          className="EntradaDeTexto"
+          disabled={
+            Desactivado ? true : TipoDeEntrada !== "select" ? true : false
+          }
+          onChange={ManejarCambio}
+          hidden={TipoDeEntrada !== "select" ? true : false}
+          value={TipoDeEntrada !== "select" ? "" : Valor}
+        >
+          {OpcionesDelDesplegable.map((item) => (
+            <option value={item.Valor}>{item.Etiqueta}</option>
+          ))}
+        </select>
+      )}
     </>
   );
 };
