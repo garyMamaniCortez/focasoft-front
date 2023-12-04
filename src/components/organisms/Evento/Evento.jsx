39345e1282b1dc92900dae9544bd9986cd612ba8
4drianC4 <camararequeadrianrafael@gmail,com>
2023-12-04 02:04:34 -0400

    fix: cambios en campos de Evento

--- src/components/organisms/Evento/Evento.jsx
+++ src/components/organisms/Evento/Evento.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 import "./Evento.css";
 
 const Evento = (props) => {
-  
+
   console.log(props.Datos.Afiche);
   let idFormulario =
     "/RegistrarParticipante/" +
@@ -54,7 +54,7 @@ const Evento = (props) => {
             <Etiqueta TipoDeEtiqueta="AtributoEvento1">Premios:</Etiqueta>
             <br />
             <Etiqueta TipoDeEtiqueta="AtributoEvento2">
-              {props.Datos.Premios.join(", ")}
+              {props.Datos.Premios}
             </Etiqueta>
           </Fondo></div>
           <div className={`${
@@ -68,7 +68,7 @@ const Evento = (props) => {
             <Etiqueta TipoDeEtiqueta="AtributoEvento1">Requisitos:</Etiqueta>
             <br />
             <Etiqueta TipoDeEtiqueta="AtributoEvento2">
-              {props.Datos.Requisitos.join(", ")}
+              {props.Datos.Requisitos}
             </Etiqueta>
           </Fondo></div>
         </div>
